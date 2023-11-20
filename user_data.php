c7a34d04f5c11ec9a9fc939131a8d8e7b3901ec4
Lyapin Yury <fraso@mail.ru>
2023-11-20 13:19:48 +0300

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -15,11 +15,10 @@
 //https://github.com/iFraso-dev/ZtarLine
 //Contacts - fraso1989@gmail.com / Fraso@mail.ru
 //
-$user_login = 'Your@email.com'; //Ваш логин от https://my.starline.ru/site/login  //Your login from https://my.starline.ru/site/login  //#example "$user_login = 'Your@email.com';"
-$user_pass = sha1('Your_password'); //Ваш пароль от https://my.starline.ru/site/login //Your password from https://my.starline.ru/site/login //#example "$user_pass = sha1('Your_password');"
+$user_login = 'Your@email.com'; //Ваш логин от https://my.starline.ru/site/login  //Your login from https://my.starline.ru/site/login  //#example "$user_login = 'user@gmail.com';"
+$user_pass = sha1('Your_password'); //Ваш пароль от https://my.starline.ru/site/login //Your password from https://my.starline.ru/site/login //#example "$user_pass = sha1('12345pass54321');"
 
-$user_AppId = 'AppId'; //Ваш сгенерированный AppId //Your generated AppId
-$user_Secret = 'Your_generated_Secret'; //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret = 'Your_generated_Secret';"
-$user_Secret_md5 = md5('Your_generated_Secret'); //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret_md5 = md5('Your_generated_Secret');"
-
+$user_AppId = 'AppId'; //Ваш сгенерированный AppId //Your generated AppId //#example "$user_AppId = '18126';"
+$user_Secret = 'Your_generated_Secret'; //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret = 'mvNWuRJadRzHlcIkIjadR9o0Tnf0iMzsCsd';"
+$user_Secret_md5 = md5('Your_generated_Secret'); //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret_md5 = md5('mvNWuRJadRzHlcIkIjadR9o0Tnf0iMzsCsd');"
 ?>
