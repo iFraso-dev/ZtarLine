fcee32785127338eb2666812fdb53798eefbb1a0
Lyapin Yury <fraso@mail.ru>
2023-11-20 12:33:02 +0300

    Create user_data.php

---
+++ user_data.php
@@ -0,0 +1,25 @@
+<?php
+//
+//__ ________                                                  __
+//|  \        \                                                |  \
+// \▓▓ ▓▓▓▓▓▓▓▓ ______   ______   _______  ______          ____| ▓▓ ______  __     __
+//|  \ ▓▓__    /      \ |      \ /       \/      \ ______ /      ▓▓/      \|  \   /  \
+//| ▓▓ ▓▓  \  |  ▓▓▓▓▓▓\ \▓▓▓▓▓▓\  ▓▓▓▓▓▓▓  ▓▓▓▓▓▓\      \  ▓▓▓▓▓▓▓  ▓▓▓▓▓▓\\▓▓\ /  ▓▓
+//| ▓▓ ▓▓▓▓▓  | ▓▓   \▓▓/      ▓▓\▓▓    \| ▓▓  | ▓▓\▓▓▓▓▓▓ ▓▓  | ▓▓ ▓▓    ▓▓ \▓▓\  ▓▓
+//| ▓▓ ▓▓     | ▓▓     |  ▓▓▓▓▓▓▓_\▓▓▓▓▓▓\ ▓▓__/ ▓▓      | ▓▓__| ▓▓ ▓▓▓▓▓▓▓▓  \▓▓ ▓▓
+//| ▓▓ ▓▓     | ▓▓      \▓▓    ▓▓       ▓▓\▓▓    ▓▓       \▓▓    ▓▓\▓▓     \   \▓▓▓
+// \▓▓\▓▓      \▓▓       \▓▓▓▓▓▓▓\▓▓▓▓▓▓▓  \▓▓▓▓▓▓         \▓▓▓▓▓▓▓ \▓▓▓▓▓▓▓    \▓
+//
+//
+//Created by iFraso-dev
+//https://github.com/iFraso-dev/ZtarLine
+//Contacts - fraso1989@gmail.com / Fraso@mail.ru
+//
+$user_login = 'Your@email.com'; //Ваш логин от https://my.starline.ru/site/login  //Your login from https://my.starline.ru/site/login  //#example "$user_login = 'Your@email.com';"
+$user_pass = sha1('Your_password'); //Ваш пароль от https://my.starline.ru/site/login //Your password from https://my.starline.ru/site/login //#example "$user_pass = sha1('Your_password');"
+
+$user_AppId = 'AppId'; //Ваш сгенерированный AppId //Your generated AppId
+$user_Secret = 'Your_generated_Secret'; //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret = 'Your_generated_Secret';"
+$user_Secret_md5 = md5('Your_generated_Secret'); //Ваш сгенерированный Secret //Your generated Secret //#example "$user_Secret_md5 = md5('Your_generated_Secret');"
+
+?>
